8ac98e53db91ad4d035250530e774392ba74214b
lucas-catto <lucascattosilva2018@gmail.com>
2024-02-20 20:10:16 -0300

    02

--- 02.js
+++ 02.js
@@ -16,3 +16,37 @@ O valor do aumento;
 O novo salário, após o aumento.   
 
 */
+
+let salario = parseFloat(prompt("Informe o salário do colaborador: "));
+
+function calculo_porcentagem (salario, porcentagem) {
+    return (salario + (salario * (porcentagem / 100)));
+}
+
+function salario_calculo (salario) {
+    
+    if (salario < 2800) {
+        return [salario, 20, (calculo_porcentagem(salario, 20)-salario), calculo_porcentagem(salario, 20)];
+    }
+    if (salario < 7000) {
+        return [salario, 15, (calculo_porcentagem(salario, 15)-salario), calculo_porcentagem(salario, 15)];
+    }
+    if (salario < 15000) {
+        return [salario, 10, (calculo_porcentagem(salario, 10)-salario), calculo_porcentagem(salario, 10)];
+    }
+
+    return [salario, 5, (calculo_porcentagem(salario, 5)-salario), calculo_porcentagem(salario, 5)];
+}
+
+console.log("| ------------------------------ |");
+console.log("| Salário:                       |");
+console.log("| ------------------------------ |");
+console.log("| Antes do reajuste: ............ R$" + salario_calculo(salario)[0].toFixed(2));
+console.log("| Percentual de aumento aplicado: "   + salario_calculo(salario)[1].toFixed(1) + "%");
+console.log("| Valor do aumento: ............. R$" + salario_calculo(salario)[2].toFixed(2));
+console.log("| O novo salário com aumento: ... R$" + salario_calculo(salario)[3].toFixed(2));
+console.log("| ------------------------------ ");
+
+/*
+console.log(calculo_porcentagem(100, 10));
+*/
