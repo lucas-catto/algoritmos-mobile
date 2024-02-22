c8e1f0f3d031f58af9f4800783086c8ae35743c1
lucas-catto <lucascattosilva2018@gmail.com>
2024-02-22 20:07:45 -0300

    04

--- 04.js
+++ 04.js
@@ -12,13 +12,21 @@ let maior_valor        = 0;
 
 if (quantidade_valores > 0) {
 
-    maior_valor = valores[0];
-
     for (let i = 0; i < quantidade_valores; i++) {
         
         valores[i] = parseInt(prompt('Informe o '+(i+1)+' valor: '));
-
     }
+
+    maior_valor = valores[0];
+
+    for (let i = 0; i < valores.length; i++) {
+
+        if (valores[i] > maior_valor) {
+            maior_valor = valores[i];
+        }
+    }
+
+alert("O maior valor digitado foi: " + maior_valor);
 }
 else {
     alert("Sem valor definido!");
